95ce27efa31474013491946c6dc05a7465523be5
Niloy Chakravorty <chakravortyniloy@gmail.com>
2023-08-05 01:57:36 +0530

    Delete back-end/build directory
    No need to commit build. TSC should create build during compile time

--- back-end/build/models/HttpError.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class HttpError extends Error {
-    constructor(message, errorCode) {
-        super(message);
-        this.code = errorCode;
-    }
-}
-exports.default = HttpError;
